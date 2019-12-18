82d7749530e69679b2583f5f0cbc2a73d9c7c6ab
dougfeltrim <dougfeltrim@gmail.com>
2019-12-18 18:43:57 -0300

    Grafico de barras comparando 2 graficos

--- visualizando_dados.py
+++ visualizando_dados.py
@@ -2,10 +2,13 @@
 
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 7, 1, 0]
+x1 = [1, 2, 3, 4, 5]
+y1 = [2, 3, 7, 1, 0]
 
-titulo = "Grafico de barras"
+x2 = [2, 4, 6, 8, 10]
+y2 = [5, 1, 3, 7, 4]
+
+titulo = "Grafico de barras 2"
 eixox = "Eixo X"
 eixoy = "Eixo Y"
 
@@ -15,7 +18,9 @@ plt.xlabel(eixox)
 plt.xlabel(eixoy)
 
 
-plt.bar(x,y)
+plt.bar(x1,y1, label = "grupo 1")
+plt.bar(x2,y2, label = "grupo 2")
+plt.legend()
 plt.show()
 
 
